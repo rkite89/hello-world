c12840cdbca5717a230d14f56de35f33910050fc
Robert Kite <rkite89@gmail.com>
2016-11-10 09:57:40 +0000

    Added a console app

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("------------");
+            Console.WriteLine("I'm thinking of a number between 1 - 10 (inclusive), what is it?");
+
+            var random = new Random();
+            int randomNumber = random.Next(1, 11);
+
+            string userGuess = Console.ReadLine();
+            while (userGuess != Convert.ToString(randomNumber))
+            {
+                int t;
+                if(!int.TryParse(userGuess, out t))
+                    Console.WriteLine("That's not even a number! Try again.");
+                else
+                {
+                    if(t >= 1 && t <= 10)
+                        Console.WriteLine("No, that's not it - guess again!");
+                    else
+                        Console.WriteLine("It's between 1 and 10 (inclusive) you numpty! Try again.");
+                }
+
+                userGuess = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Well done, you got it!");
+            Console.ReadLine();
+        }
+    }
+}
